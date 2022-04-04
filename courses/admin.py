038bb4042bb7d7906f5c9aebfd9209a499c8ecae
lherdy <le.herdy@gmail.com>
2022-04-04 14:48:52 -0300

    Online education platform.

---
+++ courses/admin.py
@@ -0,0 +1,21 @@
+from re import search
+from django.contrib import admin
+from .models import Subject, Course, Module
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fiel = {'slug': ('title',)}
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title' 'overview']
+    prepopulated_fiels = {'slug': ('title',)}
+    inlines = [ModuleInline]
+    
